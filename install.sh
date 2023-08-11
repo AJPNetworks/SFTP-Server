e17c806d4596be96836222893ebdc7dca0a624a7
AJPNetworks <alexander@ajp.network>
2023-08-10 21:49:42 -0400

    Overlooked a few issues
    Fixed the download URLs, added force to user delete, replaced all exits with returns

--- install.sh
+++ install.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 
-# Check if script is run as root or with sudo
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root or use sudo."
-  exit 1
-fi
-
-# Check if OpenSSH server is installed
-if ! command -v sshd &>/dev/null; then
-    echo "OpenSSH server is not installed. Installing..."
-    
-    # Assuming Ubuntu/Debian. Adjust for other distributions if necessary.
-    apt update
-    apt install -y openssh-server
-    
-    if ! command -v sshd &>/dev/null; then
-        echo "Error installing OpenSSH server. Exiting."
-        exit 1
-    fi
-fi
-
-# Ensure the SSH service is enabled and started
-systemctl enable ssh
-systemctl start ssh
-
-# Previous script continues from here
-adduser_url="https://raw.githubusercontent.com/AJPNetworks/SFTP-Server/main/addsftpuser"
-deluser_url="https://raw.githubusercontent.com/AJPNetworks/SFTP-Server/main/delsftpuser"
+adduser_url="https://raw.githubusercontent.com/AJPNetworks/SFTP-Server/main/bash-commands/addsftpuser"
+deluser_url="https://raw.githubusercontent.com/AJPNetworks/SFTP-Server/main/bash-commands/delsftpuser"
 
 # Extract file names from links
 adduser_filename=$(basename "$adduser_url")
